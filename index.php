74f0af0f3be24ecee32626c2792000030e21705c
Michael Beckwith <tw2113@gmail.com>
2016-07-03 02:09:58 -0500

    new routes with notes of intent

--- index.php
+++ index.php
@@ -30,4 +30,38 @@ $app->get('/[{page}]', function ($request, $response, $args) {
     return $response;
 });
 
+// View page.
+$app->get('/add/', function ($request, $response) {
+    /*
+     * Single textarea and submit button.
+     */
+});
+
+// Return JSON data version of thoughts.
+$app->get('/json/', function ($request, $response) {
+    $uri_stuff = $request->getQueryParams();
+    /*
+     * Count: default 1. Possible: any number.
+     * Orderby: default date. Possible: date, random/
+     * Order: default: DESC. Possible: ASC, DESC
+     */
+
+    /**
+     * Query based on reuqest, return json.
+     * Date, thought.
+     */
+    $data = [];
+    $newResponse = $response->withJson($data);
+
+    return $newResponse;
+});
+
+// Random thought.
+$app->get('/json/random/', function ($request, $response) {
+    $data        = [ 'time' => 'back in', 'thought' => 'let\'s go row' ];
+    $newResponse = $response->withJson($data);
+
+    return $newResponse;
+});
+
 $app->run();
